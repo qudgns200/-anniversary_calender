9de157897694374ba86fc893c0e327e297fecabd
lee byeonghoon <qudgns200@gmail.com>
2020-03-28 23:07:12 +0900

    modify source

--- ui/js/header.js
+++
@@ -1,8 +0,0 @@
-
-  window.onload = function(){
-    this.document.getElementById("header").innerHTML='<object type="text/html" data="ui/page/header.html"></object>';
-  }
-
-//   window.onload = function () {
-//       this.document.getElementById("header").innerHTML = '<section className="hero is-small is-dark is-bold"><h1 className="title">Anniversary_Calendar</h1></section>';
-// }
